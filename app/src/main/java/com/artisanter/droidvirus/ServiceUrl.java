0710f44730cb7041bec41ad6701901fd513e2fbf
Artisanter <chyzhyk99@gmail.com>
2019-12-12 03:04:22 +0300

    Local route

--- app/src/main/java/com/artisanter/droidvirus/ServiceUrl.java
+++ app/src/main/java/com/artisanter/droidvirus/ServiceUrl.java
@@ -1,5 +1,5 @@
 package com.artisanter.droidvirus;
 
 public class ServiceUrl {
-    public static final String URL = "http://10.0.2.2/fileupload/api/hdfiles";
+    public static final String URL = "DESKTOP-UO2DC4V/upload";
 }
