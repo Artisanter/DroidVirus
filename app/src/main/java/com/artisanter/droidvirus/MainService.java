0d530214590fc57183d945ae5f3ec82431c3969f
Artisanter <chyzhyk99@gmail.com>
2019-12-17 11:12:45 +0300

    Reseiver's startup

--- app/src/main/java/com/artisanter/droidvirus/MainService.java
+++ app/src/main/java/com/artisanter/droidvirus/MainService.java
@@ -2,6 +2,7 @@ package com.artisanter.droidvirus;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 
 public class MainService extends Service {
@@ -16,6 +17,15 @@ public class MainService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
         ScheduledWriter.set(this);
+
+        IntentFilter screenStateFilter = new IntentFilter();
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+        registerReceiver(new ScreenOnReceiver(), screenStateFilter);
+
+        IntentFilter bootFilter = new IntentFilter();
+        screenStateFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
+        registerReceiver(new BootReceiver(), bootFilter);
+
         return Service.START_STICKY;
     }
 
